01c63d0e7aaad56fe024fcddb517dc43128f0136
Isaac Victoria <ka_ivj@live.com>
2021-01-09 15:20:55 -0600

    Veamos si sale lo que quiero

--- mylcd.py
+++ mylcd.py
@@ -8,33 +8,33 @@ signalData = [
 ]
 waitData = [  0x00,  0x00,  0x00,  0x00,  0x00,  0x00,  0x00,  0x15]
 
-fontdata1 = [
-        # Char 0 - Upper-left
-        [ 0x00, 0x00, 0x03, 0x04, 0x08, 0x19, 0x11, 0x10 ],
-        # Char 1 - Upper-middle
-        [ 0x00, 0x1F, 0x00, 0x00, 0x00, 0x11, 0x11, 0x00 ],
-        # Char 2 - Upper-right
-        [ 0x00, 0x00, 0x18, 0x04, 0x02, 0x13, 0x11, 0x01 ],
-        # Char 3 - Lower-left
-        [ 0x12, 0x13, 0x1b, 0x09, 0x04, 0x03, 0x00, 0x00 ],
-        # Char 4 - Lower-middle
-        [ 0x00, 0x11, 0x1f, 0x1f, 0x0e, 0x00, 0x1F, 0x00 ],
-        # Char 5 - Lower-right
-        [ 0x09, 0x19, 0x1b, 0x12, 0x04, 0x18, 0x00, 0x00 ],
-        # Char 6 - my test
-	[ 0x1f,0x0,0x4,0xe,0x0,0x1f,0x1f,0x1f],
-]
-
 mylcd.lcd_display_string("Iniciando ",2,6)
-mylcd.lcd_display_string("...",3,9)
+sleep(1)
+mylcd.lcd_display_string(".",3,9)
+sleep(1)
+mylcd.lcd_display_string(".",3,10)
+sleep(1)
+mylcd.lcd_display_string(".",3,11)
+sleep(1)
+mylcd.lcd_display_string(" ",3,9)
+sleep(1)
+mylcd.lcd_display_string(" ",3,10)
+sleep(1)
+mylcd.lcd_display_string(" ",3,11)
+sleep(1)
+mylcd.lcd_display_string(".",3,9)
+sleep(1)
+mylcd.lcd_display_string(".",3,10)
+sleep(1)
+mylcd.lcd_display_string(".",3,11)
 
-sleep(5)
+sleep(1)
 
 mylcd.lcd_clear()
 
 mylcd.lcd_load_custom_chars(signalData)
-mylcd.lcd_write(0x80);
-mylcd.lcd_write_char(0)
+mylcd.lcd_display_string_pos(unichr(0),1,18)
+#mylcd.lcd_display_string("")
 
 #********************
 #******iniciando*****
