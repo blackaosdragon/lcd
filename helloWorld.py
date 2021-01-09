004c43fbaa2cd0fe67bb2040d1b20b2b212569b6
Isaac Victoria <ka_ivj@live.com>
2021-01-09 02:39:48 -0600

    Intentando colocar el hola mundo

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,8 @@
 #from smbus import SM
-print("Hello world")
+#print("Hello world")
+import I2C_LCD_driver 
+from time import *
+
+mylcd = I2C_LCD_driver()
+
+mylcd.lcd_display_string("Hola mundo!",1)
