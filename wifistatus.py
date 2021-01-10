3c0b505950fd6d879ab49f68110a92ccb8d87139
Isaac Victoria <ka_ivj@live.com>
2021-01-09 19:38:32 -0600

    A ver ...

--- wifistatus.py
+++ wifistatus.py
@@ -7,17 +7,17 @@ def get_name(cell):
     return matching_line(cell,"ESSID")[1:-1]
 
 def get_calidad(cell):
-    quality = matching_line(cell,"Quality=").split()[0].split('/')
+    calidad = matching_line(cell,"Calidad=").split()[0].split('/')
     #quality = matching_line(cell,"Quality=")     .split()[0].split('/')
-    return str(int(round(float(quality[0]) / float(quality[1]) * 100))).rjust(3) + "%"
+    return str(int(round(float(calidad[0]) / float(calidad[1]) * 100))).rjust(3) + "%"
 
 rules = {
     "Name": get_name,
-    "Quality": get_calidad
+    "Calidad": get_calidad
 }
 
 def sort_cells(cells):
-    sortby = "Quality"
+    sortby = "Calidad"
     reverse = True
     cells.sort(None, lambda el:el[sortby],reverse)
 
