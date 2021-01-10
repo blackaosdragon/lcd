145591604c619e55be50adb1ad0e8831117af6ec
Isaac Victoria <ka_ivj@live.com>
2021-01-09 21:55:58 -0600

    Modificando el orden de la señal

--- wifistatus.py
+++ wifistatus.py
@@ -17,9 +17,9 @@ def get_calidad(cell):
     quality = matching_line(cell,"Quality=").split()[0].split('/')
     #quality = matching_line(cell,"Quality=")     .split()[0].split('/')
     valor = int(round(float(quality[0]) / float(quality[1]) * 100))
-    lcd.lcd_display_string("%d"%valor,1,16)
+    lcd.lcd_display_string("%d"%valor,1,17)
     lcd.lcd_display_string("%",1,18)
-    lcd.lcd_display_string(unichr(0), 1, 19)
+    lcd.lcd_display_string(unichr(0), 1, 15)
     sleep(5)
     return str(int(round(float(quality[0]) / float(quality[1]) * 100))).rjust(3) + "%"
 
