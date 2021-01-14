841d91578deea9547ea2b0b017e49310877a141c
VictoryTheDragon <ka_ivj@live.com>
2021-01-14 16:54:21 -0600

    Bien veamos que hay al recibir la respuesta

--- mylcd.py
+++ mylcd.py
@@ -134,7 +134,7 @@ while True:
         'id': 3,
         'temperatura': temperatura
     }
-    x = requests.post(url,data=obj)
+    x = requests.post(url_local,data=obj)
     print(x.text)
 
 sleep(1)
