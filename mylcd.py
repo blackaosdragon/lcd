49f612863736f47c4afc7bb106a5a321cad8cff5
VictoryTheDragon <ka_ivj@live.com>
2021-01-14 11:38:27 -0600

    Emmmm identado corregido(?

--- mylcd.py
+++ mylcd.py
@@ -101,6 +101,6 @@ def read_temp():
 while True:
     mylcd.lcd_display_string("Temperatura: ",3,1)
     mylcd.lcd_display_string("%d"%read_temp(),3,15)
-    mylcd.lcd_display_string(unichr(3), 3, 15)
-    mylcd.lcd_display_string("C",3,16)
-	sleep(1)
+    mylcd.lcd_display_string(unichr(3), 3,17)
+    mylcd.lcd_display_string("C",3,18)
+sleep(1)
