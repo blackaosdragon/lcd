6e815b23ffd071b11fb42fa2b8f47d03e6fe1060
VictoryTheDragon <ka_ivj@live.com>
2021-01-15 17:10:23 -0600

    Varisa cosas cambian solo para hacer la peticion

--- peticionTest.js
+++ peticionTest.js
@@ -1,6 +1,32 @@
+const https = require('https')
+let url_prueba = 'instrumentacionline.ddns.net/'
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+})
+
+const options = {
+    hostname: url_prueba,
+    port: 443,
+    path: "/tomardata",
+    method: 'GET',
+    agent: httpsAgent
+}
+
+const req = https.request(options, res => {
+    console.log(`Status code: ${res.statusCode}`)
+    res.on('data', data => {
+        process.stdout.write(data)
+    });
+});
+req.on('error',e=>{
+    console.log(e)
+})
+req.end()
+/*
 const axios = require('axios');
 axios.post("https://instrumentacionline.ddns.net/tomardata").then( res => {
     console.log(res)
 }).catch( error => {
     console.log(error);
 })
+*/
