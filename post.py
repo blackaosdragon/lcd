ff4ab89325d7b8d23ef3c270b208438ba2015a09
VictoryTheDragon <ka_ivj@live.com>
2021-01-15 12:57:12 -0600

    Ahora si imprimira la hora

--- post.py
+++ post.py
@@ -15,12 +15,13 @@ def obtener_tiempo():
     #hora = reloj.hour
     #minuto = reloj.minute
     #segundo = reloj.second
-    
+    """
     if hora<0:
         hora = hora + 16
     else:
         hora = hora - 6
     print('Hora actual %d:%d '%(hora,minuto))
+    """
 while True:
     obtener_tiempo()
     time.sleep(1)
