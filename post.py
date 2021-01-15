31f2a5c1b7aa0ebe35ad302eccbc6ea8d9ed65e8
VictoryTheDragon <ka_ivj@live.com>
2021-01-15 13:37:23 -0600

    Veamos si funciona

--- post.py
+++ post.py
@@ -3,9 +3,11 @@ import requests
 import time
 import datetime
 
+url_local = 'https://instrumentacionline.ddns.net:5002/raspbi'
+
 async def main():
     try :
-        peticion = requests.get('https://github.com/timeline.json')
+        peticion = requests.post(url_local,data={temp=20, id:3.0},verify=False)
         print(peticion)
     except:
         print("Ocurrio un error")
@@ -15,19 +17,18 @@ def obtener_tiempo():
     minuto = reloj.minute
     segundo = reloj.second
     
-    """
     if hora<0:
         hora = hora + 16
     else:
         hora = hora - 6
-    """
     print('%d:%d'%(hora,minuto))
 
 
-
+"""
 while True:
     obtener_tiempo()
     time.sleep(1)
+"""
 while True:
     asyncio.run(main())
 
