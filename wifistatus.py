50ae9cc3ea116f5358cab182a7b77ad5d88bac37
VictoryTheDragon <ka_ivj@live.com>
2021-01-21 11:20:46 -0600

    Veamos cuando sale lo que quiero

--- wifistatus.py
+++ wifistatus.py
@@ -30,15 +30,15 @@ def get_calidad(cell):
     valor = int(round(float(quality[0]) / float(quality[1]) * 100))
     
     if valor==100:
-        lcd.lcd_display_string(unichr(0), 1, 17)
-        lcd.lcd_display_string(unichr(1), 1, 9)
-        lcd.lcd_display_string(unichr(2), 1, 10)
-        lcd.lcd_display_string(unichr(3), 1, 11)
-        lcd.lcd_display_string(unichr(4), 1, 12)
-        lcd.lcd_display_string(unichr(5), 1, 13)
-        lcd.lcd_display_string(unichr(6), 1, 14)
-        lcd.lcd_display_string(unichr(7), 1, 15)
-        lcd.lcd_display_string(unichr(8), 1, 16)
+        lcd.lcd_display_string(unichr(1), 1, 17)
+        lcd.lcd_display_string(unichr(2), 1, 9)
+        lcd.lcd_display_string(unichr(3), 1, 10)
+        lcd.lcd_display_string(unichr(4), 1, 11)
+        lcd.lcd_display_string(unichr(5), 1, 12)
+        lcd.lcd_display_string(unichr(6), 1, 13)
+        lcd.lcd_display_string(unichr(7), 1, 14)
+        lcd.lcd_display_string(unichr(8), 1, 15)
+        lcd.lcd_display_string(unichr(9), 1, 16)
 
     elif valor>=90 and valor<100:
         lcd.lcd_display_string(unichr(1), 1, 17)
