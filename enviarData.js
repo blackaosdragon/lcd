59bc10102205c13ad3e10791cc00e7b20f5157e3
VictoryTheDragon <ka_ivj@live.com>
2021-01-22 13:36:49 -0600

    Veamos si poniendole el while ya jala bien y ya se como corregirlo

--- enviarData.js
+++ enviarData.js
@@ -5,8 +5,10 @@ const puerto = 3001
 
 let aplicacion = spawn('python',['./test.py'])
 
-aplicacion.stdout.on('data', data => {
-    temp = data.toString()
-    console.log(temp)
+while (True){
+    aplicacion.stdout.on('data', data => {
+        temp = data.toString()
+        console.log(temp)
+    })
+}
 
-})
