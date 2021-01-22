7b897404d39b4df91518a4ea4238e12b65d42244
VictoryTheDragon <ka_ivj@live.com>
2021-01-22 11:31:42 -0600

    Upsi un poquito de vision me falta :V

--- enviarData.js
+++ enviarData.js
@@ -4,6 +4,9 @@ const app = express();
 const puerto = 3001
 
 let aplicacion = spawn('python',['./mylcd.py'])
-process.stdout.on('data',()=>{
+
+aplicacion.stdout.on('data',()=>{
     console.log(data.toString())
+    
+    console.log()
 })
