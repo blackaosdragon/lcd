29183308b32a337ee7bbc94acf6146c08a681627
Isaac Victoria <ka_ivj@live.com>
2021-01-23 20:40:18 -0600

    Enviando datos especiales

--- enviarData.js
+++ enviarData.js
@@ -20,15 +20,15 @@ setInterval(()=>{
     let fecha = new Date()
     let mes = fecha.getMonth()+1
     if(parseInt(fecha.getSeconds())%2==0){
-        lcd.println(`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`,1);
+        //lcd.println(`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`,1);
     } else {
-        lcd.println(`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()} ${fecha.getMinutes()}`,1);
+        //lcd.println(`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()} ${fecha.getMinutes()}`,1);
     }
 
 },00)
-lcd.println(` `,2);
+//lcd.println(` `,2);
 
-lcd.println(`T = ${temperatura} C`,3);
+//lcd.println(`T = ${temperatura} C`,3);
 lcd.createChar( 0,[ 0x1B,0x15,0x0E,0x1B,0x15,0x1B,0x15,0x0E] ).createChar( 1,[ 0x0C,0x12,0x12,0x0C,0x00,0x00,0x00,0x00] );
 
 
