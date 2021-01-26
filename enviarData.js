ab0c03f5b72a219ec58c730a6bbf86b6a6d677a0
VictoryTheDragon <ka_ivj@live.com>
2021-01-26 11:11:54 -0600

    Ceamos si ya se puede imprimir

--- enviarData.js
+++ enviarData.js
@@ -4,6 +4,7 @@ const lcd = new LCD( 1, 0x27, 20, 4 );
 
 let temperatura = 18.789
 lcd.beginSync();
+lcd.createCharSync( 0,[ 0x1B,0x15,0x0E,0x1B,0x15,0x1B,0x15,0x0E] ).createCharSync( 1,[ 0x0C,0x12,0x12,0x0C,0x00,0x00,0x00,0x00] );
 //lcd.createCharSync( 0,[ 0x00,0x0A,0x0A,0x1F,0x0E,0x04,0x04,0x04] )
 //const spawn = require('child_process').spawn
 
@@ -33,6 +34,7 @@ setInterval(()=>{
             lcd.printLineSync(2, `T = Error`)
         } else {
             lcd.printLineSync(2,`T = ${temp} C`,);
+            LCD.getChar(1)
         }
 
     })
