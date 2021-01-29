0835aa3000b427f6554b2acede2a1fab7d7e12d9
VictoryTheDragon <ka_ivj@live.com>
2021-01-29 10:37:23 -0600

    Creo que ya se que colocaba de manera incorrecta

--- ultasonico.js
+++ ultasonico.js
@@ -1,3 +1,4 @@
+/*
 const Gpio = require('pigpio').Gpio;
 
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
@@ -22,10 +23,39 @@ const watchHCSR04 = () => {
   });
 };
 
-
+watchHCSR04();
 
 // Trigger a distance measurement once per second
 setInterval(() => {
   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-  watchHCSR04();
+  console.log(echo)
+}, 1000);
+*/
+const Gpio = require('pigpio').Gpio;
+
+const led = new Gpio(17, {
+  mode: Gpio.OUTPUT,
+  alert: true
+});
+
+const watchLed = () => {
+  let startTick;
+
+  // Use alerts to determine how long the LED was turned on
+  led.on('alert', (level, tick) => {
+    if (level == 1) {
+      startTick = tick;
+    } else {
+      const endTick = tick;
+      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+      console.log(diff);
+    }
+  });
+};
+
+watchLed();
+
+// Turn the LED on for 15 microseconds once per second
+setInterval(() => {
+  led.trigger(15, 1);
 }, 1000);
