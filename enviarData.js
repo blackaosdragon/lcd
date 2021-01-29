ce41cc7855316cb16f236089c898e30519618c3b
VictoryTheDragon <ka_ivj@live.com>
2021-01-29 12:04:36 -0600

    Probandop cual explota primero mi server o el emisor

--- enviarData.js
+++ enviarData.js
@@ -1,25 +1,12 @@
 const redes = require('node-wifi-scanner');
 const sensor = require('ds18b20');
 const LCD = require('raspberrypi-liquid-crystal');
+const https = require('https');
 const lcd = new LCD( 1, 0x27, 20, 4 );
-
-/*
-  
-  
-  // Trigger a distance measurement once per second
-  setInterval(() => {
-     // Set trigger high for 10 microseconds
-  }, 1000);
-
-let temperatura = 18.789
-*/
-
+const fetch = require('')
 
 lcd.beginSync();
 lcd.createCharSync( 0,[ 0x1B,0x15,0x0E,0x1B,0x15,0x1B,0x15,0x0E] ).createCharSync( 1,[ 0x0C,0x12,0x12,0x0C,0x00,0x00,0x00,0x00]).createCharSync( 2,[ 0x00,0x0a,0x0a,0x0a,0x1f,0x0e,0x04,0x04] ).createCharSync( 3,[ 0xa,0xa,0xa,0x1f,0xe,0x4,0x4,0x0] ).createCharSync( 4,[ 0x1,0x1,0x3,0x3,0x7,0x7,0xf,0x1f]).createCharSync( 5,[0x0,0xd,0x11,0x11,0xd,0x11,0x10,0xd]);
-//lcd.createCharSync( 0,[ 0x00,0x0A,0x0A,0x1F,0x0E,0x04,0x04,0x04] )
-//const spawn = require('child_process').spawn
-
 /*
 sensor.sensors( (err,ids) => {
     if(err){
@@ -78,9 +65,7 @@ setInterval( ()=>{
             }
 
         })
-    }
-    
-    
+    }  
     wifisignal();
     sensor.temperature('28-011913ff6583',(err,temp)=>{
         if(err){
@@ -88,54 +73,31 @@ setInterval( ()=>{
             //lcd.printLineSync(2, `T = Error`)
         } else {
             lcd.printLineSync(2,`T = ${temp}${LCD.getChar(1)}C  `,);
+            let payload = {
+                id: 5,
+                temp: temp
+            }
+            const httpsAgent = new https.Agent({
+                rejectUnauthorized: false,
+            })
+            fetch('https://instrumentacionline.ddns.net:5002/tomardata',{
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers:{
+                'Content-Type': 'application/json' 
+                },
+            agent: httpsAgent
+            }).then(response=>{
+                return response.json();
+            }).then(data=>{
+                console.log(data);
+            }).catch((err)=>{
+                console.log("Error:");
+                console.log(err);
+            })
+
         }
 
     })
     
 },1000);
-//////////////////////////////////////
-/*
-setInterval(()=>{
-    let fecha = new Date();
-    let mes = fecha.getMonth()+1;
-    if(parseInt(fecha.getSeconds())%2==0){
-        lcd.printLineSync(0,`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`);
-    } else {
-        lcd.printLineSync(0,`${fecha.getDate()}/${mes}/${fecha.getFullYear()} ${fecha.getHours()} ${fecha.getMinutes()}`);
-    }
-    sensor.temperature('28-011913ff6583',(err,temp)=>{
-        if(err){
-            console.log(err)
-            lcd.printLineSync(2, `T = Error`)
-        } else {
-            lcd.printLineSync(2,`T = ${temp}${LCD.getChar(1)}C  `,);
-        }
-
-    })
-},1000)
-///////////////////////////////////////////////
-*/
-
-
-
-//lcd.println(` `,2);
-
-//
-
-
-
-/*
-setInterval(()=>{
-    
-    sensor.temperature('28-011913ff6583', (err,temp)=>{
-        if(err){
-            console.log("Error: ",err)
-        } else {
-            console.log(`T = ${temp}°C`);
-            lcd.print(`T = ${temp} °C`);
-        }
-    })
-},5000)
-*/
-
-
